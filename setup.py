5e9c0962ee3d464d01dcc6c2ff01468d5347a4e6
Pandentia <hi@pandentia.qcx.io>
2018-07-18 22:18:07 +0200

    Add dynamic versioning

--- setup.py
+++ setup.py
@@ -3,11 +3,23 @@ from setuptools import setup
 
 requires = [line for line in open('requirements.txt').read().strip().split('\n') if not line.startswith('http')]
 
+version = '0.0.1'
+# noinspection PyBroadException
+try:
+    import subprocess
+    p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    commit, _ = p.communicate()
+    if commit:
+        version += '+git.' + commit.decode('UTF-8').strip()
+except Exception:
+    pass
+
 setup(
     name='pylava',
     author='Pandentia',
     description='A lavalink interface for discord.py bots',
     license='MIT',
     packages=['pylava'],
-    install_requires=requires
+    install_requires=requires,
+    version=version,
 )
